ec9edb36d4210994cb25c3cbc2b891477273a4e7
Sergiotm87 <sergiotm87@gmail.com>
2017-03-10 18:08:45 +0100

    4: planteamiento y obtencion de datos a tratar

--- enunciado4.py
+++ enunciado4.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 import json
 #
-#	Introduce el nombre de un album, indica cuales albumes han sido reproducidos más veces
+#	Introduce el nombre de un album, indica cuales canciones han sido reproducidos más veces
 #
 with open('Metallica.json') as data_file:
     data = json.load(data_file)
 
-#print data
+album=raw_input("Introduce el nombre de un album: ")
+#ej. Master of Puppets,Ride the lightning
+
+#tomamos el numero de reproducciones de las canciones del album introducido
+
+for elem in data:
+    if elem["album"]["name"]==album:
+        for cancion in elem["album"]["tracks"]["track"]:
+            print cancion["streamable"]["playcount"]
+
+#pedimos por teclado cuantos resultados quiere mostrar y lo mostramos
