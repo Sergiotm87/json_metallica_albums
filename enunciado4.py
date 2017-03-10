8d1d5ab17a32cbb654a2397fb1bab56b33e37b1e
Sergiotm87 <sergiotm87@gmail.com>
2017-03-10 19:14:30 +0100

    4 resuelto

--- enunciado4.py
+++ enunciado4.py
@@ -7,7 +7,7 @@ with open('Metallica.json') as data_file:
     data = json.load(data_file)
 
 album=raw_input("Introduce el nombre de un album: ")
-#ej. Master of Puppets,Ride the lightning
+#ej. Master of Puppets,Ride the Lightning
 
 #tomamos el numero de reproducciones de las canciones del album introducido
 
@@ -17,16 +17,21 @@ listacanciones=[]
 for elem in data:
     if elem["album"]["name"]==album:
         for cancion in elem["album"]["tracks"]["track"]:
-            listareproducciones.append(cancion["streamable"]["playcount"])
-            listacanciones.append(cancion["name"])
+            reproducciones=cancion["streamable"]["playcount"]
+            tema=cancion["name"]
+            item=str(tema+" "+str(reproducciones))
+            listacanciones.append(item)
 
+#usamos una funcion lambda que sepera el ultimo elemento numerico de cada registro temporalmente para ordenar la lista descendentemente
+
+listacanciones.sort(key=lambda x:int(x.split()[-1]),reverse=True)
+
+#print listacanciones
 
 #pedimos por teclado cuantos resultados quiere mostrar y lo mostramos
 
-resultado=int(raw_input("¿cuantos resultados quieres mostrar?: \n"))
+resultado=int(raw_input("¿cuantos resultados quieres mostrar?: "))
 
 if resultado<len(listacanciones):
-    for i,j in zip(listacanciones[0:resultado],listareproducciones[0:resultado]):
-        print i+":",j,"reproducciones"
-
-#falta ordenar listas
+    for i in listacanciones[0:resultado]:
+        print i
