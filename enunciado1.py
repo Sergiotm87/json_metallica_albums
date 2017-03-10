0435858b9524dcea8b81b762844a160d6ead05ee
Sergiotm87 <sergiotm87@gmail.com>
2017-03-10 17:34:10 +0100

    1,2: resueltos

--- enunciado1.py
+++ enunciado1.py
@@ -7,10 +7,20 @@ with open('Metallica.json') as data_file:
     data = json.load(data_file)
 
 #tomamos las canciones, de las que tengan una duracion mayor a 6min tomamos su nombre y su duracion pasada a notacion minutos:segundos
+listacanciones=[]
+duracion=[]
 
-#recorremos el json tomando la duracion de las canciones, aquellas que duran mas de 6min(300)
+#recorremos el json tomando la duracion de las canciones, aquellas que duran mas de 6min(360)
 #las guardamos en una lista junto con su nombre
 
 for elem in data:
     for cancion in elem["album"]["tracks"]["track"]:
-        print cancion["duration"]
+        if int(cancion["duration"])>360:
+            listacanciones.append(cancion["name"])
+            duracion.append(str(int(cancion["duration"])/60)+":"+str(int(cancion["duration"])%60))
+print "Hay",len(listacanciones),"canciones cuya duración es mayor a 6 min:\n"
+
+#mostramos de forma paralela ambas listas mediante zip
+
+for i,j in zip(listacanciones,duracion):
+    print i,j
--- enunciado2.py
+++ enunciado2.py
@@ -10,16 +10,21 @@ with open('Metallica.json') as data_file:
 listaetiquetas=[]
 
 for elem in data:
-    for etiqueta in elem["album"]["tags"]["tag"]:
-        listaetiquetas.append(etiqueta["name"])
+    for tag in elem["album"]["tags"]["tag"]:
+        listaetiquetas.append(tag["name"])
 
-#print listaetiquetas
-for p in listaetiquetas: print p
+for p in list(set(listaetiquetas)): print p
 
 #pedimos que se introduzca alguna por teclado y recorremos los albumes en busca de la elejida
-#etiqueta=raw_input("introduce el nombre de una etiqueta")
+etiqueta=raw_input("\nintroduce el nombre de una etiqueta: ")
 
+listaalbumes=[]
 
+for elem in data:
+    for tag in elem["album"]["tags"]["tag"]:
+        if tag["name"]==etiqueta:
+            listaalbumes.append(elem["album"]["name"])
 
+print "Los albumes en los que aparece la etiqueta",etiqueta,"son:\n"
+for elem in listaalbumes: print elem
 
-#mostramos los albumes en los que aparece la etiqueta
