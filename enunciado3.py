897b02e49ec9a4246b80bbf2219ffe4110072a9e
Sergiotm87 <sergiotm87@gmail.com>
2017-03-10 17:42:01 +0100

    3: planteamiento y obtencion de datos a tratar

--- enunciado3.py
+++ enunciado3.py
@@ -6,4 +6,15 @@ import json
 with open('Metallica.json') as data_file:
     data = json.load(data_file)
 
-#print data
+tema=raw_input("introduce el nombre de una cancion: ")
+#ej. "...and Justice for All","Orion","Fade to Black"
+
+#recorremos el json en busca de estas canciones y tomamos el nombre del album correspondiente
+
+
+for elem in data:
+    for cancion in elem["album"]["tracks"]["track"]:
+        print cancion["name"]
+
+
+#print "La cancion",tema,"pertenece al album",album
